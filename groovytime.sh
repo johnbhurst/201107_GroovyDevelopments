df308b915447eac7186e15950c2030087448208b
John Hurst <john.b.hurst@gmail.com>
2011-07-18 15:08:40 +1200

    Added some Queens benchmarks.

---
+++ groovytime.sh
@@ -0,0 +1,70 @@
+rm *class
+export GROOVY_HOME=/opt/groovy-1.5.8/
+/opt/groovy-1.5.8/bin/groovyc standalone/QueensGroovy.groovy
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 18 |grep seconds  >time-1.5.8-18.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 18 |grep seconds >>time-1.5.8-18.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 18 |grep seconds >>time-1.5.8-18.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 18 |grep seconds >>time-1.5.8-18.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 18 |grep seconds >>time-1.5.8-18.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 20 |grep seconds  >time-1.5.8-20.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 20 |grep seconds >>time-1.5.8-20.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 20 |grep seconds >>time-1.5.8-20.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 20 |grep seconds >>time-1.5.8-20.txt
+java -cp /opt/groovy-1.5.8/embeddable/groovy-all-1.5.8.jar:. QueensGroovy 20 |grep seconds >>time-1.5.8-20.txt
+
+rm *class
+export GROOVY_HOME=/opt/groovy-1.6.9/
+/opt/groovy-1.6.9/bin/groovyc standalone/QueensGroovy.groovy
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 18 |grep seconds  >time-1.6.9-18.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 18 |grep seconds >>time-1.6.9-18.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 18 |grep seconds >>time-1.6.9-18.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 18 |grep seconds >>time-1.6.9-18.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 18 |grep seconds >>time-1.6.9-18.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 20 |grep seconds  >time-1.6.9-20.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 20 |grep seconds >>time-1.6.9-20.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 20 |grep seconds >>time-1.6.9-20.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 20 |grep seconds >>time-1.6.9-20.txt
+java -cp /opt/groovy-1.6.9/embeddable/groovy-all-1.6.9.jar:. QueensGroovy 20 |grep seconds >>time-1.6.9-20.txt
+
+rm *class
+export GROOVY_HOME=/opt/groovy-1.7.10/
+/opt/groovy-1.7.10/bin/groovyc standalone/QueensGroovy.groovy
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 18 |grep seconds  >time-1.7.10-18.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 18 |grep seconds >>time-1.7.10-18.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 18 |grep seconds >>time-1.7.10-18.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 18 |grep seconds >>time-1.7.10-18.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 18 |grep seconds >>time-1.7.10-18.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 20 |grep seconds  >time-1.7.10-20.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 20 |grep seconds >>time-1.7.10-20.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 20 |grep seconds >>time-1.7.10-20.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 20 |grep seconds >>time-1.7.10-20.txt
+java -cp /opt/groovy-1.7.10/embeddable/groovy-all-1.7.10.jar:. QueensGroovy 20 |grep seconds >>time-1.7.10-20.txt
+
+rm *class
+export GROOVY_HOME=/opt/groovy-1.8.0/
+/opt/groovy-1.8.0/bin/groovyc standalone/QueensGroovy.groovy
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds  >time-1.8.0-18.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds >>time-1.8.0-18.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds >>time-1.8.0-18.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds >>time-1.8.0-18.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds >>time-1.8.0-18.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds  >time-1.8.0-20.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds >>time-1.8.0-20.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds >>time-1.8.0-20.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds >>time-1.8.0-20.txt
+java -cp /opt/groovy-1.8.0/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds >>time-1.8.0-20.txt
+
+rm *class
+export GROOVY_HOME=/opt/groovypp-0.4.279/
+/opt/groovypp-0.4.279/bin/groovyc standalone/QueensGroovy.groovy
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds  >time-0.4.279-18.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds >>time-0.4.279-18.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds >>time-0.4.279-18.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds >>time-0.4.279-18.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 18 |grep seconds >>time-0.4.279-18.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds  >time-0.4.279-20.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds >>time-0.4.279-20.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds >>time-0.4.279-20.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds >>time-0.4.279-20.txt
+java -cp /opt/groovypp-0.4.279/embeddable/groovypp-all-0.4.279_1.8.0.jar:/opt/groovypp-0.4.279/embeddable/groovy-all-1.8.0.jar:. QueensGroovy 20 |grep seconds >>time-0.4.279-20.txt
+
