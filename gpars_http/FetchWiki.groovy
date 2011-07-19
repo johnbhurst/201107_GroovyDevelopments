830b721ff8c2b75ee604b016f2eb8a9f83fb1c29
John Hurst <john.b.hurst@gmail.com>
2011-07-20 07:09:42 +1200

    Added FetchWiki.groovy.

---
+++ gpars_http/FetchWiki.groovy
@@ -0,0 +1,24 @@
+
+def urls = [
+  "http://en.wikipedia.org/wiki/Groovy_(programming_language)",
+  "http://en.wikipedia.org/wiki/Object-oriented_programming",
+  "http://en.wikipedia.org/wiki/Programming_paradigm",
+  "http://en.wikipedia.org/wiki/Paradigm",
+  "http://en.wikipedia.org/wiki/Science",
+  "http://en.wikipedia.org/wiki/Knowledge",
+  "http://en.wikipedia.org/wiki/Fact",
+  "http://en.wikipedia.org/wiki/Information",
+  "http://en.wikipedia.org/wiki/Sequence",
+  "http://en.wikipedia.org/wiki/Mathematics",
+  "http://en.wikipedia.org/wiki/Quantity",
+  "http://en.wikipedia.org/wiki/Property_(philosophy)",
+  "http://en.wikipedia.org/wiki/Modern_philosophy",
+  "http://en.wikipedia.org/wiki/Philosophy"
+]
+
+urls.each {url ->
+  def filename = url.replace("http://en.wikipedia.org/wiki/", "").replace("(", "").replace(")", "") + ".html"
+  println "Fetching $url into $filename"
+  new File(filename).text = new URL(url).text
+}
+
